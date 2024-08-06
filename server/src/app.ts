b28d694df0e3ae32e28b1b10ce7c3e800069e03c
harikrishnadba <sharikrishna228@gmail.com>
2024-08-06 08:32:28 +0530

    added

---
+++ server/src/app.ts
@@ -0,0 +1,23 @@
+import express, { Express } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import propertyRoutes from './routes/userroutes';
+import sequelize from './config/database';
+
+
+const app: Express = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json()); //req.body
+
+// Routes
+app.use('/api', propertyRoutes);
+const PORT: number = parseInt(process.env.PORT as string, 10) || 5000; //port
+
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
